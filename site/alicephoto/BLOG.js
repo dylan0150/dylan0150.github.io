1e1ae34f64ea5ec9e57a57073b84a850d6ad50a6
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-08-22 05:26:55 +0100

    save

--- site/alicephoto/BLOG.js
+++ site/alicephoto/BLOG.js
@@ -8,40 +8,45 @@ app.controller('BlogController', function($scope, $routeParams) {
       intro: "My big summer project!",
       date: "14/05/2016",
       tagphoto: "image/blog/1.jpg",
-      text: [
+      content: [
         {
           heading: "Hello! So this weekend I've been busy at work getting the musicians of Manchester together to help out on my project!",
           content: "",
+          photo: {
+            location: "",
+            url: "image/blog/1.jpg",
+            comment: "[spunge] Gig",
+            date: "",
+          }
         },
+
         {
           heading: "",
           content: "So far I've got over 20 eager to help me out! Throughout my journey I will be keeping this blog, a vlog on YouTube and putting all the photos on my Facebook page!",
+          photo: [
+            {
+              location: "",
+              url: "image/blog/1.jpg",
+              comment: "[spunge] Gig",
+              date: "",
+            },
+          ]
         },
+
         {
           heading: "",
           content: "I'll see you on the next post!",
+          photo: [
+            {
+              location: "",
+              url: "image/blog/1.jpg",
+              comment: "[spunge] Gig",
+              date: "",
+            },
+          ]
         },
-      ],
-      photo: [
-        {
-          location: "",
-          url: "image/blog/1.jpg",
-          comment: "[spunge] Gig",
-          date: "",
-        },
-        {
-          location: "",
-          url: "image/blog/2.jpg",
-          comment: "Sound of Dream",
-          date: "",
-        },
-        {
-          location: "",
-          url: "image/blog/3.jpg",
-          comment: "Andrew Warner",
-          date: "",
-        },
-      ],
+
+      ]
     },
 
     {

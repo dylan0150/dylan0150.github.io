7f9dc8fe79678634fda1dd43571e694fa8d019be
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-08-22 05:28:49 +0100

    save

--- site/alicephoto/BLOG.js
+++ site/alicephoto/BLOG.js
@@ -9,15 +9,18 @@ app.controller('BlogController', function($scope, $routeParams) {
       date: "14/05/2016",
       tagphoto: "image/blog/1.jpg",
       content: [
+
         {
-          heading: "Hello! So this weekend I've been busy at work getting the musicians of Manchester together to help out on my project!",
-          content: "",
-          photo: {
-            location: "",
-            url: "image/blog/1.jpg",
-            comment: "[spunge] Gig",
-            date: "",
-          }
+          heading: "",
+          content: "So far I've got over 20 eager to help me out! Throughout my journey I will be keeping this blog, a vlog on YouTube and putting all the photos on my Facebook page!",
+          photo: [
+            {
+              location: "",
+              url: "image/blog/1.jpg",
+              comment: "[spunge] Gig",
+              date: "",
+            },
+          ]
         },
 
         {
@@ -35,7 +38,7 @@ app.controller('BlogController', function($scope, $routeParams) {
 
         {
           heading: "",
-          content: "I'll see you on the next post!",
+          content: "So far I've got over 20 eager to help me out! Throughout my journey I will be keeping this blog, a vlog on YouTube and putting all the photos on my Facebook page!",
           photo: [
             {
               location: "",
@@ -50,29 +53,3 @@ app.controller('BlogController', function($scope, $routeParams) {
     },
   ];
 })
-
-
-/* START OF EXAMPLE ENTRY
-{
-  id: 1
-  title: "",
-  intro: "",
-  date: "",
-  url: "",
-  tagphoto: "",
-  text: [
-    {
-      heading: "",
-      content: "",
-    },
-  ],
-  photo: [
-    {
-      location: "",
-      url: "",
-      comment: "",
-      date: "",
-    },
-  ],
-},
-END OF EXAMPLE ENTRY */

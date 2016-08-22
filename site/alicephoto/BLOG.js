e9f817dab9f2d579cd5dfaa257ed61f7e471717a
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-08-22 06:35:19 +0100

    save

--- site/alicephoto/BLOG.js
+++ site/alicephoto/BLOG.js
@@ -3,19 +3,19 @@ app.controller('BlogController', function($scope, $routeParams) {
   $scope.params = $routeParams.id;
   $scope.entrys = [
 
-    entry1 = {
+    {
       title: "one",
       intro: "two",
       date: "three",
       tagphoto: "",
     },
-    entry2 = {
+    {
       title: "one",
       intro: "two",
       date: "three",
       tagphoto: "",
     },
-    entry3 = {
+    {
       title: "one",
       intro: "two",
       date: "three",

0f7b3c4a9d342243bc8ac16f154b20336617866e
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-08-22 04:57:39 +0100

    save

--- site/alicephoto/BLOG.js
+++ site/alicephoto/BLOG.js
@@ -1,24 +1,73 @@
 app.controller('BlogController', function($scope, $routeParams) {
   $scope.name = 'BlogController';
   $scope.params = $routeParams;
-  $scope.entry = [
+  $scope.entrys = [
     {
-      id: 1,
-      url: "blog/blog1.html",
-      date: "",
-      title: "",
-    },
-    {
-      id: 2,
-      url: "blog/blog2.html",
-      date: "",
-      title: "",
-    },
-    {
-      id: 3,
-      url: "blog/marchblog.html",
-      date: "",
-      title: "",
+      id: 1
+      title: "The Start of a Long Summer!",
+      intro: "My big summer project!",
+      date: "14/05/2016",
+      url: "#/blog/:id",
+      tagphoto: "image/blog/1.jpg",
+      text: [
+        {
+          heading: "Hello! So this weekend I've been busy at work getting the musicians of Manchester together to help out on my project!",
+          content: "",
+        },
+        {
+          heading: "",
+          content: "So far I've got over 20 eager to help me out! Throughout my journey I will be keeping this blog, a vlog on YouTube and putting all the photos on my Facebook page!",
+        },
+        {
+          heading: "",
+          content: "I'll see you on the next post!",
+        },
+      ],
+      photo: [
+        {
+          location: "",
+          url: "image/blog/1.jpg",
+          comment: "[spunge] Gig",
+          date: "",
+        },
+        {
+          location: "",
+          url: "image/blog/2.jpg",
+          comment: "Sound of Dream",
+          date: "",
+        },
+        {
+          location: "",
+          url: "image/blog/3.jpg",
+          comment: "Andrew Warner",
+          date: "",
+        },
+      ],
     },
   ]
 })
+
+/* START OF EXAMPLE ENTRY
+{
+  id: 1
+  title: "",
+  intro: "",
+  date: "",
+  url: "",
+  tagphoto: "",
+  text: [
+    {
+      heading: "",
+      content: "",
+    },
+  ],
+  photo: [
+    {
+      location: "",
+      url: "",
+      comment: "",
+      date: "",
+    },
+  ],
+},
+END OF EXAMPLE ENTRY */

e469f314370c1d8783e70d9757439302e582abf1
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-08-22 07:13:30 +0100

    save

--- site/alicephoto/BLOG.js
+++ site/alicephoto/BLOG.js
@@ -4,36 +4,48 @@ app.controller('BlogController', function($scope, $routeParams) {
   $scope.entrys = [
 
     {
-      title: "one",
-      intro: "two",
-      date: "three",
-      tagphoto: "",
+      title: "The Start Of A Long Summer!",
+      intro: "My big summer project!",
+      date: "14/05/2016",
+      tagphoto: "image/blog/face.jpg",
       content: [
         {
-          heading: "heading",
-          text: "text",
+          heading: "",
+          text: "Hello! So this weekend I've been busy at work getting the musicians of Manchester together to help out on my project!",
           photos: [
             {
-              location: "location",
-              url: "",
-              comment: "comment",
-              date: "date",
+              location: "",
+              url: "image/blog/1.jpg",
+              comment: "[spunge] Gig",
+              date: "",
+            },
+          ],
+        },
+        {
+          heading: "",
+          text: "So far I've got over 20 eager to help me out! Throughout my journey I will be keeping this blog, a vlog on YouTube and putting all the photos on my Facebook page!",
+          photos: [
+            {
+              location: "",
+              url: "image/blog/2.jpg",
+              comment: "Sound Of Dream",
+              date: "",
+            },
+          ],
+        },
+        {
+          heading: "",
+          text: "I'll see you on the next post!",
+          photos: [
+            {
+              location: "",
+              url: "image/blog/3.jpg",
+              comment: "Andrew Warner",
+              date: "",
             },
           ],
         },
       ],
-    },
-    {
-      title: "one",
-      intro: "two",
-      date: "three",
-      tagphoto: "",
-    },
-    {
-      title: "one",
-      intro: "two",
-      date: "three",
-      tagphoto: "",
     },
 
   ];

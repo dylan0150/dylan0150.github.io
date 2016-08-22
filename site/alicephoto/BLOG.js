608bca2d5c9f4d5761ef8bd90dd79a029a2d2967
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-08-22 05:18:06 +0100

    save

--- site/alicephoto/BLOG.js
+++ site/alicephoto/BLOG.js
@@ -44,6 +44,88 @@ app.controller('BlogController', function($scope, $routeParams) {
       ],
     },
 
+    {
+      title: "The Start of a Long Summer!",
+      intro: "My big summer project!",
+      date: "14/05/2016",
+      tagphoto: "image/blog/1.jpg",
+      text: [
+        {
+          heading: "Hello! So this weekend I've been busy at work getting the musicians of Manchester together to help out on my project!",
+          content: "",
+        },
+        {
+          heading: "",
+          content: "So far I've got over 20 eager to help me out! Throughout my journey I will be keeping this blog, a vlog on YouTube and putting all the photos on my Facebook page!",
+        },
+        {
+          heading: "",
+          content: "I'll see you on the next post!",
+        },
+      ],
+      photo: [
+        {
+          location: "",
+          url: "image/blog/1.jpg",
+          comment: "[spunge] Gig",
+          date: "",
+        },
+        {
+          location: "",
+          url: "image/blog/2.jpg",
+          comment: "Sound of Dream",
+          date: "",
+        },
+        {
+          location: "",
+          url: "image/blog/3.jpg",
+          comment: "Andrew Warner",
+          date: "",
+        },
+      ],
+    },
+
+    {
+      title: "The Start of a Long Summer!",
+      intro: "My big summer project!",
+      date: "14/05/2016",
+      tagphoto: "image/blog/1.jpg",
+      text: [
+        {
+          heading: "Hello! So this weekend I've been busy at work getting the musicians of Manchester together to help out on my project!",
+          content: "",
+        },
+        {
+          heading: "",
+          content: "So far I've got over 20 eager to help me out! Throughout my journey I will be keeping this blog, a vlog on YouTube and putting all the photos on my Facebook page!",
+        },
+        {
+          heading: "",
+          content: "I'll see you on the next post!",
+        },
+      ],
+      photo: [
+        {
+          location: "",
+          url: "image/blog/1.jpg",
+          comment: "[spunge] Gig",
+          date: "",
+        },
+        {
+          location: "",
+          url: "image/blog/2.jpg",
+          comment: "Sound of Dream",
+          date: "",
+        },
+        {
+          location: "",
+          url: "image/blog/3.jpg",
+          comment: "Andrew Warner",
+          date: "",
+        },
+      ],
+    },
+
   ]
 })
 

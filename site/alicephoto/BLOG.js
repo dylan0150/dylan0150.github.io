5076196a28948682761a0322d0d748c9f780152e
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-08-22 06:37:54 +0100

    save

--- site/alicephoto/BLOG.js
+++ site/alicephoto/BLOG.js
@@ -1,6 +1,6 @@
 app.controller('BlogController', function($scope, $routeParams) {
   $scope.name = 'BlogController';
-  $scope.params = $routeParams.id;
+  $scope.params = $routeParams;
   $scope.entrys = [
 
     {

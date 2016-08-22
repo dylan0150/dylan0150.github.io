9d2c5a4a9abdafa71d0e13c246d7544475855018
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-08-22 05:19:21 +0100

    save

--- site/alicephoto/js/app.js
+++ site/alicephoto/js/app.js
@@ -24,7 +24,7 @@ app.config(function ($routeProvider) {
     })
     .when('/blog/:id', {
       controller: 'BlogController',
-      templateUrl: 'blog/:id'
+      templateUrl: 'blog/blog.html'
     })
 });
 

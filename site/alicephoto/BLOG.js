837f9ee73739e3eb8f032d29e48a3144a4866394
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-08-22 05:37:19 +0100

    save

--- site/alicephoto/BLOG.js
+++ site/alicephoto/BLOG.js
@@ -48,5 +48,95 @@ app.controller('BlogController', function($scope, $routeParams) {
       ],
     },
 
+    {
+      title: "The Start of a Long Summer!",
+      intro: "My big summer project!",
+      date: "14/05/2016",
+      tagphoto: "image/blog/1.jpg",
+      content: [
+        {
+          heading: "",
+          content: "So far I've got over 20 eager to help me out! Throughout my journey I will be keeping this blog, a vlog on YouTube and putting all the photos on my Facebook page!",
+          photo: [
+            {
+              location: "",
+              url: "image/blog/1.jpg",
+              comment: "[spunge] Gig",
+              date: "",
+            },
+          ]
+        },
+        {
+          heading: "",
+          content: "So far I've got over 20 eager to help me out! Throughout my journey I will be keeping this blog, a vlog on YouTube and putting all the photos on my Facebook page!",
+          photo: [
+            {
+              location: "",
+              url: "image/blog/1.jpg",
+              comment: "[spunge] Gig",
+              date: "",
+            },
+          ]
+        },
+        {
+          heading: "",
+          content: "So far I've got over 20 eager to help me out! Throughout my journey I will be keeping this blog, a vlog on YouTube and putting all the photos on my Facebook page!",
+          photo: [
+            {
+              location: "",
+              url: "image/blog/1.jpg",
+              comment: "[spunge] Gig",
+              date: "",
+            },
+          ]
+        },
+      ],
+    },
+
+    {
+      title: "The Start of a Long Summer!",
+      intro: "My big summer project!",
+      date: "14/05/2016",
+      tagphoto: "image/blog/1.jpg",
+      content: [
+        {
+          heading: "",
+          content: "So far I've got over 20 eager to help me out! Throughout my journey I will be keeping this blog, a vlog on YouTube and putting all the photos on my Facebook page!",
+          photo: [
+            {
+              location: "",
+              url: "image/blog/1.jpg",
+              comment: "[spunge] Gig",
+              date: "",
+            },
+          ]
+        },
+        {
+          heading: "",
+          content: "So far I've got over 20 eager to help me out! Throughout my journey I will be keeping this blog, a vlog on YouTube and putting all the photos on my Facebook page!",
+          photo: [
+            {
+              location: "",
+              url: "image/blog/1.jpg",
+              comment: "[spunge] Gig",
+              date: "",
+            },
+          ]
+        },
+        {
+          heading: "",
+          content: "So far I've got over 20 eager to help me out! Throughout my journey I will be keeping this blog, a vlog on YouTube and putting all the photos on my Facebook page!",
+          photo: [
+            {
+              location: "",
+              url: "image/blog/1.jpg",
+              comment: "[spunge] Gig",
+              date: "",
+            },
+          ]
+        },
+      ],
+    },
+
   ];
 })

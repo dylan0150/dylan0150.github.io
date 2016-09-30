3a47c0cbfd9512269ebc4378143dd4f1e470209c
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-09-30 04:43:13 +0100

    save

--- site/quickproject/js/controller/memberscontroller.js
+++ site/quickproject/js/controller/memberscontroller.js
@@ -3,19 +3,19 @@ app.controller('MembersController', function($scope, $routeParams) {
   $scope.params = $routeParams;
   $scope.members = [
     {
-      name: "John",
-      games: 24,
-      joindate: "01/01/2010",
+      name: "John";
+      games: 24;
+      joindate: "01/01/2010";
     },
     {
-      name: "John",
-      games: 24,
-      joindate: "01/01/2010",
+      name: "John";
+      games: 24;
+      joindate: "01/01/2010";
     },
     {
-      name: "John",
-      games: 24,
-      joindate: "01/01/2010",
+      name: "John";
+      games: 24;
+      joindate: "01/01/2010";
     }
   ]
 })

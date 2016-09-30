41511379324a6eb07b600ff6172e08a68acef564
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-09-30 06:22:36 +0100

    save

--- site/quickproject/js/controller/memberscontroller.js
+++ site/quickproject/js/controller/memberscontroller.js
@@ -12,29 +12,40 @@ app.controller('MembersController', function($scope, $routeParams) {
           score: "",
           win: "",
           date: "",
+          opponent: "",
         },
       ],
-      gethighscore: function(){
-        var highscore = 0;
-        for (var i = 0; i < games.length; i++) {
-          if (games[i].win) {
-            if (games[i].score > highscore) {
-              highscore = games[i].score;
-            }
-          }
-        }
-        return {
-          score: highscore;
-        }
-      },
-      getaveragescore: function(){
-        var totalscore = 0;
-        for (var i = 0; i < games.length; i++) {
-          totalscore += games[i];
-        }
-        return Math.floor(totalscore / games[i].length);
-      },
-
     }
   ];
 })
+
+gethighscore = function(val,member){
+  for (var i = 0; i < member.games.length; i++) {
+    if (member.games[i].win) {
+      if (member.games[i].score > highscore) {
+        var highscore = member.games[i].score;
+        var when = member.games[i].date;
+        var who = member.games[i].opponent;
+      }
+    }
+  };
+  switch (val) {
+    case "score":
+      return highscore;
+    break;
+    case "date":
+      return when;
+    break;
+    case "opponent":
+      return who;
+    break;
+  };
+};
+
+getaveragescore = function(member){
+  var totalscore = 0;
+  for (var i = 0; i < member.games.length; i++) {
+    totalscore += member.games[i];
+  }
+  return Math.floor(totalscore / member.games[i].length);
+};

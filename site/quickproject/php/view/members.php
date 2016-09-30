40ee7e83137cd54fcfaabd8fe4040350b2e56c44
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-09-30 04:16:25 +0100

    save

--- site/quickproject/php/view/members.php
+++ site/quickproject/php/view/members.php
@@ -4,16 +4,16 @@
     <td><b>No. of Games</b></td>
     <td><b>Join Date</b></td>
   </th>
-  <a href="#/members/:1"><tr>
+  <tr onclick="window.document.location='#/members/:1'">
     <td>1</td>
     <td>John</td>
     <td>26</td>
     <td>05/07/1995</td>
-  </tr></a>
-  <a href="#/members/:2"><tr>
+  </tr>
+  <tr onclick="window.document.location='#/members/:2'">
     <td>2</td>
     <td>Alex</td>
     <td>126</td>
     <td>04/11/2001</td>
-  </tr></a>
+  </tr>
 </table>

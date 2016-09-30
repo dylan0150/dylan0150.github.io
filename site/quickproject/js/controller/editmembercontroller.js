4648d0287e224456a090735f7469b87b434eb7e0
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-09-30 04:36:55 +0100

    route

--- site/quickproject/js/controller/editmembercontroller.js
+++
@@ -1,4 +0,0 @@
-app.controller('EditMemberController', function($scope, $routeParams) {
-  $scope.name = 'EditMemberController';
-  $scope.params = $routeParams;
-})
--- site/quickproject/js/controller/memberscontroller.js
+++ site/quickproject/js/controller/memberscontroller.js
@@ -1,4 +1,21 @@
 app.controller('MembersController', function($scope, $routeParams) {
   $scope.name = 'MembersController';
   $scope.params = $routeParams;
+  $scope.members = [
+    {
+      name: "John",
+      games: 24,
+      joindate: "01/01/2010",
+    };
+    {
+      name: "John",
+      games: 24,
+      joindate: "01/01/2010",
+    };
+    {
+      name: "John",
+      games: 24,
+      joindate: "01/01/2010",
+    };
+  ]
 })

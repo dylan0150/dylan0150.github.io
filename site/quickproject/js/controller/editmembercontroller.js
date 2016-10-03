4fd4443ab2b3527e17c23f241765e18d34a92dbc
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-10-03 16:55:52 +0100

    save

--- site/quickproject/js/controller/editmembercontroller.js
+++ site/quickproject/js/controller/editmembercontroller.js
@@ -2,9 +2,5 @@ app.controller('EditMembersController', function($scope, $routeParams, $http) {
   $scope.name = 'EditMembersController';
   $scope.params = $routeParams;
 
-  $scope.update = function(){
-    $http.post(){
-      
-    }
-  }
+  
 })

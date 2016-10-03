57e46b6cf1d520378cd7b9961076d308f1f30cff
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-10-03 16:06:44 +0100

    save

--- site/quickproject/php/getmembers.php
+++ site/quickproject/php/getmembers.php
@@ -1,32 +1,5 @@
 <?php
-header("Content-Type: application/json; charset=ISO-8859-1");
+include "dbconnect.php";
 
-$servername="";
-$username="";
-$password="";
-$dbname="";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-$rs = $conn->execute("SELECT FirstName, LastName, JoinDate, Age, Games FROM Members");
-
-$outp = "";
-while (!$rs->EOF) {
-    if ($outp != "") {$outp .= ",";}
-    $outp .= '{"firstname":"'  . $rs["FirstName"] . '",';
-    $outp .= '"lastname":"'   . $rs["LastName"]        . '",';
-    $outp .= '"age":"'   . $rs["Age"]        . '",';
-    $outp .= '"games":"'   . $rs["Games"]        . '",';
-    $outp .= '"joindate":"'. $rs["JoinDate"]     . '"}';
-    $rs->MoveNext();
-}
-$outp ='{"records":['.$outp.']}';
-
-$conn->close();
-
-echo ($outp);
+$data = json_decode(file_get_contents("php://input"));
 ?>

3e812c42d14d3a142794135be30e2059113ee0e5
Dylan Hanner <dylan.hanner@yahoo.co.uk>
2017-06-22 17:17:27 +0100

    save

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ var express     = require('express')
 var bodyParser  = require('body-parser')
 var fs          = require('fs')
 var uuid        = require('node-uuid')
+var Client      = require('mariasql')
+
+var c = new Client({
+  host: 'localhost',
+  user: 'foo',
+  password: 'bar'
+})
+
+c.query('SHOW DATABASES', function(err, rows) {
+  if (err) throw err;
+  console.dir(rows);
+})
+
+c.end();
 
 var path = process.argv[1].split('index.js')[0]
 
